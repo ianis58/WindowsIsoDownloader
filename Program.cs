c176cdbfe777d08354ea83a459ce751a56fa2fc9
Ianis <ianis58@gmail.com>
2023-02-27 17:30:34 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using PuppeteerSharp;
+using WindowsIsoDownloader;
+
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+var browser = await Puppeteer.LaunchAsync(new LaunchOptions());
+
+var pages = await browser.PagesAsync();
+var page = pages.First();
+
+// Opening the main Windows 11 download page
+await page.GoToAsync("https://www.microsoft.com/en-us/software-download/windows11/");
+
+// Selecting the Windows edition
+await page.SelectAsync("#product-edition", "2370");
+
+// Clicking on the button to validate the Windows edition
+await page.ClickAsync("#submit-product-edition");
+
+// Selecting the Windows language (waiting some time to be sure the page is ready)
+await page.WaitForTimeoutAsync(2500);
+await page.SelectAsync("#product-languages", """{"id":"14965","language":"English"}""");
+
+// Clicking on the button to validate the Windows language
+await page.ClickAsync("#submit-sku");
+
+// Obtaining the iso download link (waiting some time to be sure the page is ready)
+await page.WaitForTimeoutAsync(4500);
+var hrefFromLinks = await page.QuerySelectorAllHandleAsync("#card-info-content .button").EvaluateFunctionAsync<string[]>("elements => elements.map(a => a.href)");
+
+if(hrefFromLinks.Any() && hrefFromLinks.First().Contains(".iso"))
+{
+    var isoFileUri = hrefFromLinks.First();
+    
+    using(var httpClient = new HttpClient())
+    {
+        httpClient.Timeout = TimeSpan.FromHours(2);
+
+        using (var filestream = new FileStream("windows11.iso", FileMode.Create, FileAccess.Write, FileShare.None))
+        {
+            var progress = new Progress<float>();
+            progress.ProgressChanged += ProgressChanged;
+            await httpClient.DownloadAsync(isoFileUri, filestream, progress);
+        }
+    }
+
+}
+else
+{
+    Console.WriteLine("Error: download button was not found.");
+}
+
+void ProgressChanged(object? sender, float e)
+{
+    Console.WriteLine($"ISO download progress: {e.ToString("p1")}  ");
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Windows Iso Downloader
+
+Windows Iso Downloader is a tool that allows to programatically download the latest Windows 11 ISO file from Microsoft servers.
+It can be usefull in projects that allows you to build a custom installer for Windows 11, to reduce manual actions.
+
+## Building
+1. Build it with the "dotnet build" command
+2. Run the produced WindowsIsoDownloader.exe
+3. The result will be a windows11.iso file in the same folder as the WindowsIsoDownloader.exe
+
+## Why not publishing a release?
+I prefer not to publish a pre-built executable. The goal is to have as little code as possible,
+so that it's very easy to audit it by reading the sources.
